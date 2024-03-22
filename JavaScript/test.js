bd495fa42e4342b41c526da39fed6b22c107604a
Paulotican <paulodaniel317@gmail.com>
2024-03-21 22:36:20 -0300

    primeiro commit html-css

---
+++ JavaScript/test.js
@@ -0,0 +1,52 @@
+/*alert('Óla, seja bem vindo, todo bem?')
+//document.getElementByiId('nome').value = 'oi'
+
+strig
+var texto = 'Curso de JS'
+
+	-------Variavel------
+number
+var numeroInteiro = -7
+var numeroFracionado = 101.75
+
+boolean
+true ->verdadeira / false -> falso 
+var teste = true
+
+//alert(numeroFracionado) / abrium dialog
+console.log(numeroInteiro)
+
+	-------Concatenação--------
+var nome = prompt('Digite seu nome:')
+var idade = prompt('Digite sua idade:')
+var idade1 = 50
+
+console.log(idade)
+console.log(idade1)
+
+document.write('<h1>olá ' + nome + ', tudo bem? Estou vendo que você possi ' +idade+ ' anos </H1>')*/
+
+			// -----Variáveis null e undefined----
+
+// null
+	var teste = null
+
+//undefined
+	var teste2 = undefined
+
+	console.log(teste)
+	console.log(teste2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
